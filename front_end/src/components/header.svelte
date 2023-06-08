b05b2bb1672cb4f3ad4db71bc584913609e2d29e
Sh1nku <42642351+Sh1nku@users.noreply.github.com>
2023-06-08 23:00:27 +0200

    Fix mobile header size issue. Add cache to docker images.

--- front_end/src/components/header.svelte
+++ front_end/src/components/header.svelte
@@ -4,7 +4,7 @@
 
 
 <header class="relative flex items-center justify-between">
-        <div class="flex items-center flex-shrink-0 text-black mr-6">
+        <div class="flex items-center flex-shrink-0 text-black">
             <a href="/">
                 <img class="inline-block" alt="logo" src="/favicon.png" />
                 <span class="font-mono uppercase align-middle text-xl tracking-tight">badgestore.dev</span>
--- front_end/src/components/navbar.svelte
+++ front_end/src/components/navbar.svelte
@@ -2,16 +2,16 @@
     import {env} from '$env/dynamic/public';
 </script>
 
-<nav class="flex items-center justify-between flex-shrink-0 p-6">
+<nav class="flex items-center justify-between flex-shrink-0 px-4 py-6">
     <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-sm lg:flex-grow">
-            <a href='/disclaimers' class="font-mono inline-block mt-0 pr-4 text-black hover:text-gray-600">
+            <a href='/disclaimers' class="font-mono inline-block mt-0 pr-2 md:pr-4 text-black hover:text-gray-600">
                 Disclaimers
             </a>
-            <a href={env.PUBLIC_API_PATH} class="font-mono inline-block mt-0 pr-4 text-black hover:text-gray-600">
+            <a href={env.PUBLIC_API_PATH} class="font-mono inline-block mt-0 pr-2 md:pr-4 text-black hover:text-gray-600">
                 API
             </a>
-            <div class="inline-block mt-0 p-4" >|</div>
+            <div class="inline-block mt-0 pr-2 md:pr-4" >|</div>
             <a href="https://github.com/Sh1nku/badgestore" class="inline-block mt-0 align-middle">
                 <img src="/github-mark.svg" alt="Project github page" class="object-cover h-4"/>
             </a>
